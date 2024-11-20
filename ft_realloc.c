ada33e6cf64c9d6971c9ad8b5322efdd572edf78
Tom Jooris <tjooris@z2r4p6.42lyon.fr>
2024-11-20 15:41:53 +0100

    the first

--- ft_realloc.c
+++ ft_realloc.c
@@ -1,12 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_realoc.c                                        :+:      :+:    :+:   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjooris <tjooris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/20 10:11:59 by tjooris           #+#    #+#             */
-/*   Updated: 2024/11/20 10:12:03 by tjooris          ###   ########.fr       */
+/*   Created: 2024/11/20 13:01:41 by tjooris           #+#    #+#             */
+/*   Updated: 2024/11/20 15:12:34 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+char *ft_realloc(char *src,char *src2, int size)
+{
+    char    *strnew;
+    size_t  i;
+
+    strnew = (char*)malloc(sizeof(char) * (ft_strlen(src) + ft_strlen(src2) + 1));
+    if (!strnew)
+        return (NULL);
+
+}
